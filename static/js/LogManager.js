dffe9198c2c9505525dc55c327a0950250e3f658
Gabriel Sampaio <gsampaio@redhat.com>
2024-05-08 11:10:31 -0300

    Refactoring log manager

--- static/js/LogManager.js
+++ static/js/LogManager.js
@@ -7,7 +7,7 @@ export class LogManager {
     }
 
     calculateMaxMessages() {
-        const MESSAGE_HEIGHT = 60; // Increased height to accommodate hidden details
+        const MESSAGE_HEIGHT = 60;  // Increased height to accommodate hidden details
         return Math.floor(this.container.clientHeight / MESSAGE_HEIGHT);
     }
 
@@ -24,95 +24,72 @@ export class LogManager {
         };
     }
 
-    createLogMessageElement(id, message) {
+    createLogMessageElement(id, fields) {
         const element = document.createElement('div');
-        element.className = `log-message ${this.getMessageType(message)}`;
+        element.className = `log-message ${fields.severity.toLowerCase()}`;
         element.id = `log-message-${id}`;
-        element.innerHTML = this.formatMessage(id, message);
+        element.innerHTML = this.formatMessage(id, fields);
         return element;
     }
 
-    appendLogMessage(element) {
-        if (this.container.firstChild) {
-            this.container.insertBefore(element, this.container.firstChild);
-        } else {
-            this.container.appendChild(element);
-        }
+    appendAndAnimateLogMessage(element) {
+        // Insert before the first child or append to container if empty
+        this.container.firstChild ? this.container.insertBefore(element, this.container.firstChild) : this.container.appendChild(element);
+
+        // Find the element that needs animation and start it
+        const eventMessageDiv = element.querySelector('.log-event-message');
+        this.animateText(eventMessageDiv, eventMessageDiv.textContent);
     }
 
-    setupMessageEvents(id, element, networkManager) {
-        element.addEventListener('mouseover', () => networkManager.updateNodeColor(id, 'highlightColor'));
-        element.addEventListener('mouseout', () => networkManager.updateNodeColor(id, 'defaultColor'));
+    setupMessageEvents(id, element) {
+        element.addEventListener('mouseover', () => this.networkManager.updateNodeColor(id, 'highlightColor'));
+        element.addEventListener('mouseout', () => this.networkManager.updateNodeColor(id, 'defaultColor'));
     }
 
     addLogMessage(id, message) {
-        const messageDiv = this.createLogMessageElement(id, message);
-
-        // Extract the log message fields
         const fields = this.extractFieldsFromMessage(message);
+        const messageDiv = this.createLogMessageElement(id, fields);
+        messageDiv.classList.toggle('error-flash', fields.severity === 'ERROR' || fields.severity === 'CRITICAL');
 
-        if (fields.severity === 'ERROR' || fields.severity === 'CRITICAL') {
-            messageDiv.classList.add('error-flash'); // Add the class if the log is critical or an error
-        }
+        this.appendAndAnimateLogMessage(messageDiv);
+        this.setupMessageEvents(id, messageDiv);
 
-        // Start the animation for the event message part
-        const eventMessageDiv = messageDiv.querySelector('.log-event-message');
-        const fullText = eventMessageDiv.textContent;
-        eventMessageDiv.textContent = ''; // Ensure it's empty before starting animation
-        this.animateText(eventMessageDiv, fullText);
-
-        if (this.container.firstChild) {
-            this.container.insertBefore(messageDiv, this.container.firstChild);
-        } else {
-            this.container.appendChild(messageDiv);
-        }
-
-
-        // Add event listeners for hovering
-        this.setupMessageEvents(id, messageDiv, this.networkManager);
-
-        // Return the extracted fields from the log message as an object
         return fields;
     }
 
     getMessageType(message) {
-        if (message.includes('CRITICAL')) {
-            return 'critical';
-        } else if (message.includes('WARNING')) {
-            return 'warning';
-        } else {
-            return 'info'; // Default to info if no other type matches
-        }
+        if (message.includes('CRITICAL')) return 'critical';
+        if (message.includes('WARNING')) return 'warning';
+        return 'info';
     }
 
-    formatMessage(id, message) {
-        const parts = message.split('|');
-        const datetime = parts[0].trim();
-        const eventName = parts[1].trim();
-        const level = parts[2].trim();
-        const details = parts.slice(3, parts.length - 2).join(' | ').trim();
-        const eventMessage = parts[parts.length - 3].trim();
-        const source = parts[parts.length - 2].split(':')[1].trim();
-        const businessType = parts[parts.length - 1].split(':')[1].trim();
+    formatMessage(id, fields) {
+        // Use object destructuring to extract field values
 
+        const { datetime, serviceName, eventMessage, businessType, details, source } = fields;
+
+        // Construct the HTML string using template literals and the extracted values
         return `
-            <div class="log-message-header">
-                <strong>${eventName}</strong>
-                <div class="log-timestamp">#${id} - ${datetime}</div>
-                <div class="log-event-message">${eventMessage} - ${businessType}</div>
-            </div>
-            <div class="log-details hidden">
-                ${details}
-            </div>
-            <img src="/static/media/${source.toLowerCase().replace(/\s+/g, '')}.png" class="log-source-logo" alt="${source} logo">
-        `;
+        <div class="log-message-header">
+            <strong>${serviceName}</strong>
+            <div class="log-timestamp">#${id} - ${datetime}</div>
+            <div class="log-event-message">${eventMessage} - ${businessType}</div>
+        </div>
+        <div class="log-details hidden">${details}</div>
+        <img src="/static/media/${source.toLowerCase().replace(/\s+/g, '')}.png" class="log-source-logo" alt="${source} logo">
+    `;
     }
 
-    animateText(element, text, index = 0) {
-        if (index < text.length) {
-            element.textContent += text.charAt(index);
-            setTimeout(() => this.animateText(element, text, index + 1), 50); // Adjust timing to your liking
-        }
+    animateText(element, text) {
+        element.textContent = ''; // Clear the element's content before starting the animation
+        let index = 0; // Start from the first character
+        const animate = () => {
+            if (index < text.length) {
+                element.textContent += text.charAt(index);
+                index++;
+                setTimeout(animate, 50); // Adjust timing to your liking
+            }
+        };
+        animate();
     }
-
 }
